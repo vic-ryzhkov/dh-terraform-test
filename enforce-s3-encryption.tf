b8d7b6192d43a2e6512a76fb298c41588cb68f21
Victor Ryzhkov <vic@MacBook-Pro-Victor.local>
2023-03-07 18:43:23 +0300

    Logging and code comments added

--- enforce-s3-encryption.tf
+++ enforce-s3-encryption.tf
@@ -1,24 +1,32 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "4.57.0"
-#     }
-#   }
-# }
-
-## AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_PROFILE, AWS_CONFIG_FILE, AWS_SHARED_CREDENTIALS_FILE
+# 1. Initialize AWS provider
+# Note: Set the following parameters as an environment variables or in ~/.terraform/config file 
+# AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION
 provider "aws" {
 
 }
 
-resource "aws_s3_bucket" "mybucket" {
-  bucket_prefix = "test-bucket-eks-cluster-"
+
+# 2. Set name for a new bucket 
+# Note: Unique id will be added to the bucket name automatically
+resource "aws_s3_bucket" "test_bucket" {
+  bucket_prefix = "test-bucket-"
 }
 
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.mybucket.id
+# 3. Make it private as an AWS Security Best Practice
+resource "aws_s3_bucket_public_access_block" "test_block_public_access" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+# 4. Create a bucket with a Server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "test_encryption" {
+  bucket = aws_s3_bucket.test_bucket.id
   
   rule {
     apply_server_side_encryption_by_default {
